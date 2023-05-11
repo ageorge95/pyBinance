cd9f5bf71d759bb087248afb341017eecdfe44ea
ageorge95 <arteni.george.daniel@gmail.com>
2023-05-11 16:47:41 +0300

    expanded the check_API_key() to check for the API secret as well

--- Binance/utils.py
+++ Binance/utils.py
@@ -3,8 +3,8 @@ from typing import AnyStr
 
 def check_API_key(func):
     def inner(*args, **kwargs):
-        if not args[0].API_key:
-            args[0]._log.error(f'{func.__name__} requires an API key !')
+        if not args[0].API_key or not args[0].API_secret:
+            args[0]._log.error(f'{func.__name__} requires an API key and API secret!')
         return func(*args, **kwargs)
     return inner
 
