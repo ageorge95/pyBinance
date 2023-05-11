70b4e5724e527ab8c6be151de50a18217203080c
ageorge95 <arteni.george.daniel@gmail.com>
2023-05-11 13:21:42 +0300

    added initial setup files

---
+++ Install.bat
@@ -0,0 +1,29 @@
+@echo off
+
+set root=%cd%
+
+:: installation
+
+python -m venv venv
+:: Windows doesn't allow the creation of symlinks without special priviledges, so hardlinks are created instead.
+mklink /h activate.bat venv\Scripts\activate.bat
+
+call activate.bat
+
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
+
+:: post-installation message
+
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo.
+echo pyBinance install complete.
+echo.
+echo Run 'activate' to activate pyBinance's Python virtual environment and
+echo 'deactivate' to, well, deactivate it.
+echo.
+echo Refer to EXAMPLE.py on how to actually use the API endpoints.
+echo.
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+deactivate
