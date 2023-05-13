44f23e6c99a7fec67f51700173b4850e6c6841fb
ageorge95 <arteni.george.daniel@gmail.com>
2023-05-13 19:16:10 +0300

    removed an unused import

--- Binance/endpoints/market.py
+++ Binance/endpoints/market.py
@@ -2,7 +2,6 @@ from logging import getLogger
 from typing import AnyStr,\
     List
 from Binance.network_wrappers import API_call
-from Binance.utils import check_API_key
 
 class MarketEndpoints():
     _log: getLogger
